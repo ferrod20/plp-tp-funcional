e330a350baba9bfd4c807e3ff476f14273ac9f93
validarj <validarj@fd43f41a-d239-0410-bf0e-65fcb77b3f6d>
2007-09-20 17:51:34 +0000

    acomode las pruebas

--- trunk/codigo/Reversi.hs
+++ trunk/codigo/Reversi.hs
@@ -35,7 +35,10 @@ dim (Tablero dim pos) = dim
 color (Tablero dim pos) c = pos c
 fColor (Tablero dim pos) = pos
 
--------------------------------------------------------------------------------  De aca en adelante son todas funciones nuestras
+-----------------------------------------------------------------------------------------------------------------------
+---------------------------------------------funciones del TP - Modulo reversi-----------------------------------------
+-----------------------------------------------------------------------------------------------------------------------
+
 enRango :: Coordenada -> Dimension -> Bool
 enRango   (a,b) (c,d) = a<c && b<d && a>=0 && b>=0
 
@@ -47,7 +50,7 @@ dimension :: Juego -> Dimension
 dimension = foldJuego id  f2
 	where f2 r c = r
 
--------------------------------------------------------------------------------  coordsQueInvierte 
+-------------------------------------------------------------------------------  coordsQueInvierte------------------ 
 --Idea:
 --1. Tomo los puntos mas cercanos del mismo color que estan alineados ortogonalemente o diagonalemente al punto que quiero insertar.
 --O sea, obtengo los puntos entre los cuales quedarían encerradas las piezas del otro color.
@@ -99,19 +102,20 @@ sonConsecutivas (df,dc) (hf,hc) xs = and [elem (f,c) xs| f<-[(min df hf)..(max d
 negado Blanco = Negro
 negado Negro = Blanco
 
--------------------------------------------------------------------------------  coordsQueInvierte 
+-------------------------------------------------------------------------------  puedeJugarEn------------------------------------------
 --puedeJugarEn k t c 
 --Da True si la coordenada c está en el rango del tablero t, si no hay nada en la coordenada c y si ubicar un color k en la coordenada c invierte al menos una coordenada del color opuesto.
 puedeJugarEn :: Color -> Tablero -> Coordenada -> Bool
 puedeJugarEn k (Tablero d pos) c = (enRango c d) && ((pos c) == Nothing) && length( coordsQueInvierte k t c ) > 0
 	where t = (Tablero d pos)
 
+	
+------------------------------------------------------------------------------- terminoElJuego----------------------------------------
+	
 --Da True si no hay ninguna movida para realizar. Se fija en todas las posiciones si se puede poner una ficha blanca o una negra.
 terminoElJuego :: Juego -> Bool
 terminoElJuego j = not(hayMovidasPosibles Blanco (tablero j) ) && not(hayMovidasPosibles Negro (tablero j) )
 
---and [not(algunoJuega j f c)  |f<-[0..fst(dimension j)],c<-[0..snd(dimension j)]]
---	where algunoJuega j f c = puedeJugarEn Blanco (tablero j) (f,c) || puedeJugarEn Negro (tablero j) (f,c)
 		
 hayMovidasPosibles :: Color->Tablero ->Bool
 hayMovidasPosibles col (Tablero (n,m) pos)  = or [puedeJugarEn col t (f,c)  |f<-[0..(n-1)],c<-[0..(m-1)]]
@@ -143,11 +147,16 @@ tabCol  j = foldJuego f1 f2 j
  	where	f2 (col, t) coord = modificar col t coord
 		f1 dim = ( Negro, tableroInicial dim)
 
+		
+---------------------------------------------tablero -----------------------------------------
 tablero :: Juego -> Tablero
 tablero j = snd (tabCol j)
 
+---------------------------------------------turno--------------------------------------
 turno :: Juego -> Color
 turno j | not( terminoElJuego j )= fst (tabCol j) 
+
+---------------------------------------------movidasValidas------------------------------------
 
 --Se fija en cada uno de las posiciones y de cada una de todas las posibles movidas para el turno actual en el tablero actual, arma un juego.
 movidasValidas :: Juego -> [Juego]
@@ -155,13 +164,20 @@ movidasValidas j
 	| terminoElJuego j = []
 	| otherwise = [ Poner j (f,c) | f<-[0..fst(dimension j)],c<-[0..snd(dimension j)], puedeJugarEn (turno j) (tablero j) (f,c)]
 
+	
+----------------------------------------------quienGano---------------------------------------
+	
 quienGano :: Juego -> Maybe Color
 quienGano j | (diferenciaNegrasBlancas j) > 0 = Just Negro
 			| (diferenciaNegrasBlancas j) < 0 = Just Blanco
 			| (diferenciaNegrasBlancas j) == 0 = Nothing
 
+-----------------------------------------------ultimaCoordJugada---------------------------
+			
 ultimaCoordJugada :: Juego -> Maybe Coordenada
 ultimaCoordJugada = foldJuego (\x->Nothing) (\r c->Just c)
+
+-----------------------------------------------diferenciaNegrasBlancas---------------------------
 
 diferenciaNegrasBlancas :: Juego -> Int
 diferenciaNegrasBlancas j = (contar Negro (tablero j )) - (contar Blanco ( tablero j ))
@@ -270,16 +286,24 @@ diferenciaNegrasBlancas j = (contar Negro (tablero j )) - (contar Blanco ( table
 -- uCJ1 = ultimaCoordJugada jPrueba 	 --resultado esperado Just (3,4)
 -- uCJ2 = ultimaCoordJugada ( Comenzar (6,6) ) --resultado esperado Nothing
 
--------Armado de pruebas varias
+-------Pruebas diferenciaNegrasBlancas
 
--- jPrueba2 = Poner (Poner (Poner(Poner( Poner (Comenzar (3,4)) (0,3)) (1,3))(2,1))(1,0)) (2,0)
--- jTerminado = (Poner( Poner jPrueba2 (0,0))(2,3))
--- --modificar
--- --nuevasPosic
--- --tabCol
--- --tablero
--- --turno
--- --movidasValidas
--- --diferenciaNegrasBlancas
--- --terminoElJuego
--- --quienGano
+--	dnb1 = diferenciaNegrasBlancas j1
+--	dnb2 = diferenciaNegrasBlancas j2
+
+-------Pruebas quienGano
+
+--	qg1 = quienGano j1 
+--	qg2 = quienGano j2
+
+-------Pruebas movidasValidas
+
+--	mv1 = movidasValidas j1
+--	mv2 = movidasValidas j2
+
+-------Armado de juegos de pruebas varios
+ 
+--NOTA , poner los ejemplos 
+
+-- j1 = (Poner (Poner (Poner (Poner ( Poner (Comenzar (3,4)) (0,3)) (1,3))(2,1))(1,0)) (2,0) )
+-- j2 = (Poner( Poner j2 (0,0))(2,3))
